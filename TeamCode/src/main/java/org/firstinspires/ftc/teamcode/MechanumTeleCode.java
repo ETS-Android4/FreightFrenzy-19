6f9daad11ae20588350b3248e3389f1cc66cc663
MenakaW <MeniRoshi@hotmail.com>
2021-10-17 16:57:17 -0700

    10/17 --added base teleop--

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumTeleCode.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class MechanumTeleCode extends LinearOpMode {
+
+    DcMotor backLeftMotor;
+    DcMotor frontRightMotor;
+    DcMotor backRightMotor;
+    DcMotor frontLeftMotor;
+
+    public void initialize(){
+        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRight");
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeft");
+        backRightMotor = hardwareMap.get(DcMotor.class, "backRight");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeft");
+
+
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    private void mecanumDrive(double scale){
+
+        double radius=Math.hypot(gamepad2.left_stick_x,gamepad2.left_stick_y);
+        double angle = (Math.atan2(-(gamepad2.left_stick_y),(gamepad2.left_stick_x)))-(Math.PI/4);
+        double rotation = gamepad2.right_stick_x * 0.5;
+        double fLPower = 0;
+        double bLPower = 0;
+        double fRPower = 0;
+        double bRPower = 0;
+
+        if( (angle > 5*(Math.PI/12))&& (angle < 7*(Math.PI/12)) ){
+            fLPower = radius * Math.cos(Math.PI/4) + rotation;
+            bLPower = radius * Math.sin(Math.PI/4) + rotation;
+            fRPower = radius * Math.sin(Math.PI/4) - rotation;
+            bRPower = radius * Math.cos(Math.PI/4) - rotation;
+        }
+        else if( (angle < -5*(Math.PI/12))&& (angle > -7*(Math.PI/12)) ){
+            fLPower = radius * Math.cos(-3*Math.PI/4) + rotation;
+            bLPower = radius * Math.sin(-3*Math.PI/4) + rotation;
+            fRPower = radius * Math.sin(-3*Math.PI/4) - rotation;
+            bRPower = radius * Math.cos(-3*Math.PI/4) - rotation;
+        }
+        else {
+            fLPower = radius * Math.cos(angle) + rotation;
+            bLPower = radius * Math.sin(angle) + rotation;
+            fRPower = radius * Math.sin(angle) - rotation;
+            bRPower = radius * Math.cos(angle) - rotation;
+
+        }
+        frontLeftMotor.setPower((fLPower) * scale);
+        backLeftMotor.setPower((bLPower) * scale);
+        frontRightMotor.setPower((fRPower) * scale);
+        backRightMotor.setPower((bRPower) * scale);
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initialize();
+        waitForStart();
+        while(opModeIsActive()){
+            mecanumDrive(0.6);
+            idle();
+        }
+    }
+}
