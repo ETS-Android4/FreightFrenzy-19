64d6f404ee9b820b363f41ca4e36bd217b0c8af1
sashwat2006 <sashwat2006@gmail.com>
2021-10-24 17:06:56 -0700

    mk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumTeleCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumTeleCode.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+@TeleOp(name="drive", group="teleop")
 
 public class MechanumTeleCode extends LinearOpMode {
 
