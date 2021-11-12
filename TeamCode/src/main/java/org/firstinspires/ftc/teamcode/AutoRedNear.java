72b97a1584ac7d8b9828e1d26ae6060c0f82bc4d
MenakaW <MeniRoshi@hotmail.com>
2021-11-12 15:44:15 -0800

    11/12 finished opencv

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedNear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedNear.java
@@ -6,9 +6,13 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
 @Autonomous(name="AUTO RED NEAR", group="auto")
 
@@ -18,8 +22,10 @@ public class AutoRedNear extends LinearOpMode {
     DcMotor frontRight;
     DcMotor backLeft;
     DcMotor backRight;
+    OpenCvCamera webcam;
     float header;
 
+    DetectionHelper pipeline;
     NavigationHelper navigate = new NavigationHelper();
     BNO055IMU imu;
 
@@ -27,6 +33,27 @@ public class AutoRedNear extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
 
         initialize();
+
+        DetectionHelper.DuckPosition position = DetectionHelper.DuckPosition.RIGHT;
+        while(!opModeIsActive()) {
+
+            telemetry.addData("Analysis", pipeline.getAnalysis());
+
+            position = pipeline.getPosition();
+            if(position== DetectionHelper.DuckPosition.LEFT){
+                telemetry.addData("Position", "LEFT");
+            }
+            else if(position == DetectionHelper.DuckPosition.CENTER){
+                telemetry.addData("Position", "CENTER");
+            }
+            else if(position == DetectionHelper.DuckPosition.RIGHT){
+                telemetry.addData("Position", "RIGHT");
+            }
+            telemetry.update();
+            // Don't burn CPU cycles busy-looping in this sample
+            sleep(50);
+        }
+
         waitForStart();
 
         telemetry.addData("entering loop", imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle);
@@ -63,10 +90,6 @@ public class AutoRedNear extends LinearOpMode {
     }
 
     public void initialize(){
-        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
-        backRight = hardwareMap.get(DcMotor.class, "backRight");
 
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
         parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
@@ -75,6 +98,52 @@ public class AutoRedNear extends LinearOpMode {
         parameters.mode = BNO055IMU.SensorMode.IMU;
         imu = hardwareMap.get(BNO055IMU.class,"imu");
         imu.initialize(parameters);
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+
+
+        try {
+            webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+            pipeline = new DetectionHelper();
+            webcam.setPipeline(pipeline);
+            webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+            {
+                @Override
+                public void onOpened()
+                {
+                    webcam.startStreaming(320,240, OpenCvCameraRotation.UPRIGHT);
+                }
+                @Override
+                public void onError(int errorCode)
+                {
+                    /*
+                     * This will be called if the camera could not be opened
+                     */
+                }
+            });
+
+        } catch (Exception bad){
+            telemetry.addData("EXCEPTION (from try-catch):", bad.getMessage());
+            bad.printStackTrace();
+            telemetry.update();
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        telemetry.addLine("FINISHED INITIALIZING WEBCAM");
+        telemetry.update();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
 
         frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DetectionHelper.java
@@ -0,0 +1,159 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public  class DetectionHelper extends OpenCvPipeline {
+        /*
+         * An enum to define the skystone position
+         */
+        public enum DuckPosition
+        {
+            LEFT,
+            CENTER,
+            RIGHT
+        }
+
+        /*
+         * Some color constants
+         */
+        static final Scalar BLUE = new Scalar(0, 0, 255);
+        static final Scalar GREEN = new Scalar(0, 255, 0);
+
+        /*
+         * The core values which define the location and size of the sample regions
+         */
+        //x181
+        static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(65,135);
+        static final Point REGION2_TOPLEFT_ANCHOR_POINT = new Point(245,135);
+
+        static final int REGION_WIDTH = 35;
+        static final int REGION_HEIGHT = 25;
+
+        final int FOUR_RING_THRESHOLD = 140;
+        final int ONE_RING_THRESHOLD = 135;
+
+        Point region1_pointA = new Point(
+                REGION1_TOPLEFT_ANCHOR_POINT.x,
+                REGION1_TOPLEFT_ANCHOR_POINT.y);
+        Point region1_pointB = new Point(
+                REGION1_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
+                REGION1_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
+
+        Point region2_pointA = new Point(
+                REGION2_TOPLEFT_ANCHOR_POINT.x,
+                REGION2_TOPLEFT_ANCHOR_POINT.y);
+        Point region2_pointB = new Point(
+                REGION2_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH,
+                REGION2_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
+
+        /*
+         * Working variables
+         */
+        Mat region1_Cb;
+        Mat region2_Cb;
+        Mat YCrCb = new Mat();
+        Mat Cb = new Mat();
+        int avg1;
+        int avg2;
+
+        // Volatile since accessed by OpMode thread w/o synchronization
+        private volatile boolean positionCenter = false;
+        private volatile boolean positionRight = false;
+
+        /*
+         * This function takes the RGB frame, converts to YCrCb,
+         * and extracts the Cb channel to the 'Cb' variable
+         */
+        void inputToCb(Mat input)
+        {
+            Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
+            Core.extractChannel(YCrCb, Cb, 1);
+        }
+
+        @Override
+        public void init(Mat firstFrame)
+        {
+            inputToCb(firstFrame);
+
+            region1_Cb = Cb.submat(new Rect(region1_pointA, region1_pointB));
+            region2_Cb = Cb.submat(new Rect(region2_pointA, region2_pointB));
+        }
+
+        @Override
+        public Mat processFrame(Mat input)
+        {
+            inputToCb(input);
+
+            avg1 = (int) Core.mean(region1_Cb).val[0];
+            avg2 = (int) Core.mean(region2_Cb).val[0];
+
+            Imgproc.rectangle(
+                    input, // Buffer to draw on
+                    region1_pointA, // First point which defines the rectangle
+                    region1_pointB, // Second point which defines the rectangle
+                    BLUE, // The color the rectangle is drawn in
+                    2); // Thickness of the rectangle lines
+
+            Imgproc.rectangle(
+                    input, // Buffer to draw on
+                    region2_pointA, // First point which defines the rectangle
+                    region2_pointB, // Second point which defines the rectangle
+                    BLUE, // The color the rectangle is drawn in
+                    2); // Thickness of the rectangle lines
+
+            positionCenter = false; // Record our analysis
+            if(avg1 > FOUR_RING_THRESHOLD) {
+                positionCenter = true;
+            } else{
+                positionCenter = false;
+            }
+
+            positionRight = false; // Record our analysis
+            if(avg2 > FOUR_RING_THRESHOLD) {
+                positionRight = true;
+            } else{
+                positionRight = false;
+            }
+
+
+            Imgproc.rectangle(
+                    input, // Buffer to draw on
+                    region1_pointA, // First point which defines the rectangle
+                    region1_pointB, // Second point which defines the rectangle
+                    GREEN, // The color the rectangle is drawn in
+                    -1); // Negative thickness means solid fill
+
+            Imgproc.rectangle(
+                    input, // Buffer to draw on
+                    region2_pointA, // First point which defines the rectangle
+                    region2_pointB, // Second point which defines the rectangle
+                    GREEN, // The color the rectangle is drawn in
+                    -1); // Negative thickness means solid fill
+
+            return input;
+        }
+
+        public DuckPosition getPosition() {
+            if(positionCenter){
+                return DuckPosition.CENTER;
+            }
+            else if (positionRight){
+                return DuckPosition.RIGHT;
+            }
+            else{
+                return DuckPosition.LEFT;
+            }
+        }
+
+        public int[] getAnalysis()
+        {
+            int[] analysis = {avg1, avg2};
+            return analysis;
+        }
+    }
